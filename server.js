0b0ce091a267d554ef48f4952d40af0c7b840e56
thunderenlight <thunderenlight@gmail.com>
2024-02-28 20:13:06 -0600

    Connecting intital DB.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-
+const mongoose = require("mongoose")
 const app = express();
 
 app.use(express.json())
@@ -18,7 +18,15 @@ app.post("/name", (req,res) => {
         return res.status(400).json({ error: "No name was provided." })
     }
 })
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("Connected to DB");
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`)
+    app.listen(process.env.PORT, () => {
+        console.log(`Server running on port ${process.env.PORT}`)
+})
+
+})
+.catch((err) => {
+    console.log(err)
 });
